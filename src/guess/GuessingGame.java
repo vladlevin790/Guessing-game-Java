553ecf2052b628e12e964ba8cb1c90455ff02eb6
Vlad Levin <vladlevin790@gmail.com>
2024-07-10 21:45:51 +0300

    some-updates

--- src/guess/GuessingGame.java
+++ src/guess/GuessingGame.java
@@ -15,7 +15,11 @@ public class GuessingGame extends JFrame {
 	private final JLabel lblNewLabel = new JLabel("Vlad's Guessing Game");
 	private JTextField txtGuess;
 	private JLabel lblOutput;
+	private JButton btnGuess = new JButton("Guess !!");
+	private JButton btnPlayAgain = new JButton("Play Again!!");
 	private int theNumber;
+	private int tries;
+	private int numberOfTries;
 	public GuessingGame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("Hi-Lo Guessing Game");
@@ -39,14 +43,25 @@ public class GuessingGame extends JFrame {
 		getContentPane().add(txtGuess);
 		txtGuess.setColumns(10);
 		
-		JButton btnGuess = new JButton("Guess !!");
 		btnGuess.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				checkGuess();
 			}
 		});
-		btnGuess.setBounds(167, 166, 117, 25);
+		btnGuess.setBounds(170, 166, 117, 25);
+		btnGuess.setSize(120,30);
 		getContentPane().add(btnGuess);
+		
+	
+		btnPlayAgain.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				newGame();
+			}
+		});
+		btnPlayAgain.setBounds(167, 166, 117, 25);
+		btnPlayAgain.setSize(120,30);
+		btnPlayAgain.setVisible(false);
+		getContentPane().add(btnPlayAgain);
 		
 		lblOutput = new JLabel("Enter a number above and click Guess !!");
 		lblOutput.setHorizontalAlignment(SwingConstants.CENTER);
@@ -62,7 +77,11 @@ public class GuessingGame extends JFrame {
 	}
 	
 	public void newGame() {
+		btnPlayAgain.setVisible(false);
+		btnGuess.setVisible(true);
 		theNumber = (int)(Math.random() * 100 + 1);
+		tries = 7;
+		numberOfTries = 0;
 	}
 	
 	public void checkGuess() {
@@ -70,11 +89,26 @@ public class GuessingGame extends JFrame {
 		String message = "";
 		try {
 			int guess = Integer.parseInt(guessText);
-			if (guess < theNumber) message = guess + " is too low. Try again.";
-			else if (guess > theNumber) message = guess + " is too high, Try again";
+			if (guess < theNumber && tries <= 7 && tries > 0) {
+				message = guess + " is too low. Try again.";
+				tries--;
+				numberOfTries++;
+			}
+			else if (guess > theNumber && tries <= 7 && tries > 0) {
+				message = guess + " is too high, Try again";
+				tries--;
+				numberOfTries++;
+			}
 			else {
-				message = guess + " is correct. You win. Let's play again!";
-				newGame();
+				if (tries < 7 && tries > 0) {
+					message = guess + " is correct. You win after " + numberOfTries + " tries. Let's play again!";
+					btnGuess.setVisible(false);
+					btnPlayAgain.setVisible(true);
+				} else {
+					message = "You're lost. Let's play again";
+					btnGuess.setVisible(false);
+					btnPlayAgain.setVisible(true);
+				}
 			}
 		} catch (Exception e) {
 			message = "Enter a whole number between 1 and 100";
